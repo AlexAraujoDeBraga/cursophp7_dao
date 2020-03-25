5b4956c7ed8bea7782768ac2c734224e2bc1013c
Alex Braga <bragadeveloper@gmail.com>
2020-03-25 14:27:39 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php
-
     require_once("config.php");
+    echo "carrega um usuário específico";
 
     $root = new Usuario();
     $root->loadById(1);
-
     echo $root;
+    echo "</br>";
+
+    echo "Carrega lista de todos os usuários";
+    $lista = Usuario::getList();
+    echo json_encode($lista);
+    echo "</br>";
+
+    echo "Carrega somente os usuários que tenham iniciais jo";
+    $search = Usuario::search("jo");
+    echo json_encode($search);
+    echo "</br>";
+    
+    echo "Carrega um usuário usando o login e a senha";
+    $usuario = new Usuario();
+    $usuario->login("joao", "123");
+    echo $usuario;
+    echo "</br>";
 
 ?>
