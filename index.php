37fece0fb88b61c540ff9d848fbc38cab4811b89
Alex Braga <bragadeveloper@gmail.com>
2020-03-25 15:38:47 -0300

    comando delete

--- index.php
+++ index.php
@@ -23,4 +23,24 @@
     echo $usuario;
     echo "</br>";
 
+    echo "insert de usuário novo";
+    $aluno = new Usuario("aluno","@aluno0");
+    $aluno->insert();
+    echo $aluno;
+    echo "</br>";
+
+    echo "update de usuario";
+    $usuario2 = new Usuario();
+    $usuario2->loadById(3);
+    $usuario2->update("professor","vamoslá!9090");
+    echo $usuario2;
+    echo "</br>";
+
+    echo "deletar usuario";
+    $usuario3 = new Usuario();
+    $usuario3->loadById(4);
+    $usuario3->delete();
+    echo $usuario3;
+    echo "</br>";
+
 ?>
